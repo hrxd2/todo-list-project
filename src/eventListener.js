03260a65b094e6c7c5719d696eb2b58d420592ee
hrx <171131135+hrxd2@users.noreply.github.com>
2025-12-18 07:30:51 +0000

    add: articles main title updated
    a h1 with section-title class added for main section title display

--- src/eventListener.js
+++ src/eventListener.js
@@ -4,16 +4,21 @@ import { getProjectArray, getTaskArray } from "./taskController";
 export default function asideListener(){
 
     const aside = document.querySelector(".aside");
+    const sectionTitle = document.querySelector(".section-title");
 
     aside.addEventListener("click", e => {
         if(e.target.classList.contains("all-tasks")){
             clearDisplay();
+            sectionTitle.textContent = 'All Tasks';
             getTaskArray().forEach(item => populateTasks(item));
         };
 
         if(e.target.classList.contains("project-title-button")){
             clearDisplay();
             const prjArray = getProjectArray();
+            const textValue = e.target.textContent;
+            sectionTitle.textContent = textValue;
+
             const id = e.target.dataset.uid;
             console.log(id);
                 prjArray.forEach(obj => {
